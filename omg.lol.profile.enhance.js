a97fde4792e4eb0a2470fd66d515a4363fb7502e
Bye <bye@byecorps.com>
2023-03-12 15:30:22 +0000

    Update :D

---
+++ omg.lol.profile.enhance.js
@@ -0,0 +1,54 @@
+let recent_lfm = document.getElementById('recent_lfm');
+let url = 'https://lfm.byemc.xyz/byeemc'
+
+let updateTracks = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data = data.recenttracks.track
+
+        // clear the div
+        recent_lfm.innerHTML = '';
+
+        let current = document.createElement('div');
+        current.classList.add('current');
+
+        // first item in the list is the current track
+        let currentTrack = data[0];
+        if (currentTrack['@attr'] && currentTrack['@attr'].nowplaying == 'true') {
+            let currentTrackName = `<i class="fa-fw fa-solid fa-play"></i> ${currentTrack.name}`;
+        } else {
+            let currentTrackName = currentTrack.name;
+        }
+        let currentTrackArtist = currentTrack.artist['#text'];
+        let currentTrackImage = currentTrack.image[3]['#text'];
+
+        current.innerHTML = `<img src="${currentTrackImage}" alt=""><div class="text"><h4>${currentTrackName}</h4><p>${currentTrackArtist}</p></div>`;
+
+        recent_lfm.appendChild(current);
+
+        // rest of the list is the recent tracks, which share are added to a div, but do have their own divs
+
+        let recent = document.createElement('div');
+        recent.classList.add('recent');
+
+        for (let i = 1; i < data.length; i++) {
+            let track = data[i];
+            let trackName = track.name;
+            let trackArtist = track.artist['#text'];
+            let trackImage = track.image[3]['#text'];
+
+            let trackDiv = document.createElement('div');
+            trackDiv.classList.add('track');
+
+            trackDiv.innerHTML = `<img src="${trackImage}" alt=""><h4>${trackName}</h4><p>${trackArtist}</p>`;
+
+            recent.appendChild(trackDiv);
+        }
+
+        recent_lfm.appendChild(recent);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', updateTracks);
