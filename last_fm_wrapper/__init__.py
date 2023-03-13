a535a66212e70ef23a2422f30f2aba0882e8f021
Bye <bye@byecorps.com>
2023-03-13 19:23:44 +0000

    >:C

--- last_fm_wrapper/__init__.py
+++ last_fm_wrapper/__init__.py
@@ -10,9 +10,9 @@ load_dotenv()
 
 app = Flask(__name__)
 
-with open("cors.allowed", "r") as f:
-    allowed_origins = f.read().splitlines()
-    CORS(app=app, origins=allowed_origins)
+# with open("cors.allowed", "r") as f:
+#     allowed_origins = f.read().splitlines()
+#     CORS(app=app, origins=allowed_origins)
 
 
 lfm = lastfm.LastFM(os.getenv("LASTFM_API_KEY"), os.getenv("LASTFM_API_SECRET"))
@@ -26,5 +26,14 @@ def getinfo(username):
     if request_type == "recent" or request_type is None:
         response = jsonify(lfm.get_user_recent_tracks(username, limit=4))
 
-    response.headers.add("Access-Control-Allow-Origin", "https://bye.omg.lol")
+    allowed_origins = [
+        "https://bye.omg.lol",
+        # for testing
+        "http://localhost:3000",
+    ]
+
+    if request.headers.get("Origin") in allowed_origins:
+        response.headers.add(
+            "Access-Control-Allow-Origin", request.headers.get("Origin")
+        )
     return response
